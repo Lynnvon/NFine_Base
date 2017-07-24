f0ab3908d098797cfd85c6c09d579b4f8b164cfa
lynn <632865163@qq.com>
2017-07-24 11:37:55 +0800

    Add
    Base NFine Framework

---
+++ project/NFine.ApplicationTests/LiveManage/LiveAppTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFine.ApplicationTests.LiveManage
+{
+    class LiveAppTests
+    {
+    }
+}
